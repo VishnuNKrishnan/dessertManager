45ca95216efd03af9e295abb20b453f91e40368a
Vishnu Navaneeth Krishnan <vishnunavaneet@gmail.com>
2022-04-27 23:42:24 +0400

    Completed Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState, useContext } from 'react'
 import './App.css'
+import AppWrapper from './Components/AppWrapper'
+import { UserContext } from './UserContext'
 
 function App() {
-  return <div className="App"></div>
+  const [dessertList, setDessertList] = useState([])
+  const [activePage, setActivePage] = useState('addDesert')
+  const [menuStatus, setMenuStatus] = useState('active')
+  return (
+    <UserContext.Provider
+      value={{
+        activePage,
+        setActivePage,
+        dessertList,
+        setDessertList,
+        menuStatus,
+        setMenuStatus,
+      }}
+    >
+      <div className="App">
+        <AppWrapper />
+      </div>
+    </UserContext.Provider>
+  )
 }
 
 export default App
